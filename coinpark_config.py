4b5ea36353b01406d973a3f190d1f8e035040056
jbooktrader <danjor@163.com>
2018-06-26 15:24:18 +0800

    3

--- coinpark_config.py
+++ coinpark_config.py
@@ -1,16 +1,16 @@
 ###############################参数定义##############################################
 #客户名称
-clientname = 'sunlc-eth'
+clientname = 'sunlc-BTC'
 # API KEY
 apikey = '389b943c6fd40485303d5c453657403454a30f10'
 secretkey = '10b750ad8c28e5469ec45a9da93d8499a3aa2eac'
 # 刷单速度，0.5-超慢速 1-慢速  2-中速  3-快速
-speed=3
+speed=0.1
 # 起始货币数量
-startamount = 15
-startmoney = 10197
+startamount = 0.5
+startmoney = 3000
 # 交易货币
-tradecurrency = 'ETH'
+tradecurrency = 'BTC'
 # 基础币种
 basecurrency = 'USDT'
 # 交易对
